f2c9a3db2c47ff09aaec99de7b8642791662440b
awesome-sparsh <sparshsethi@outlook.com>
2025-05-28 21:01:17 +0530

    csrf change

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'WeatherApp/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://weatherapp-3mig.onrender.com',
+]
